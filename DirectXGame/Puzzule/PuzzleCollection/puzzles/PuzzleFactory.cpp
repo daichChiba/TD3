1a03d70db277af88ee57da487e9f902c598bf9cc
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2025-03-03 16:00:12 +0900

    ファイルをまとめた

--- DirectXGame/Puzzule/PuzzleCollection/puzzles/PuzzleFactory.cpp
+++ DirectXGame/Puzzule/PuzzleCollection/puzzles/PuzzleFactory.cpp
@@ -1,5 +1,5 @@
 #include "PuzzleFactory.h"
-#include "PuzzleCollection.h"
+#include "../PuzzleCollection.h"
 
 std::unique_ptr<PuzzleCollection> PuzzleFactory::CreatePuzzle15() {
 	return std::unique_ptr<PuzzleCollection>();
